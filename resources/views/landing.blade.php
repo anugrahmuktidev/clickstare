bb966d0dc81434fbaadac05551e3bf0650f98d88
anugrahmuktidev <anugrah.mukti@unja.ac.id>
2025-10-24 13:42:52 +0700

    tittle

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -5,7 +5,7 @@
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <title>ClicSTARe — Edukasi Bahaya Rokok</title>
+  <title>ClicSTARe — Edukasi Bahaya Rokok Elektrik</title>
   @vite('resources/css/app.css')
   <style>
     html {
